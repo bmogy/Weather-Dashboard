02439a33927f865cc973d7c516ba016b417a6fae
Brian Mogy <bmogy@outloook.com>
2019-11-25 17:15:31 -0600

    I finished the weather dashboard application minus the css. The current day, 5 day forcast, and search buttons from prior searches have been created.I will start on the css later this weel

--- js/src.js
+++ js/src.js
@@ -1,53 +1,112 @@
-
-function todayWeather(){
-    var dt = new Date() 
-    var search = $("#search").val()
+var buttonArray;  
+if(localStorage.getItem("buttons") !== null){
+buttonArray = JSON.parse(localStorage.getItem("buttons"))
+}else{
+buttonArray =[]
+}
+function todayWeather(search) {
+    var dt = new Date()
     $.ajax({
         url: "http://api.openweathermap.org/data/2.5/weather?q=" + search + "&apikey=028bd6c65fc449153d91ad43fd573ddb",
         method: "GET"
     }).then(function (responce) {
         console.log(responce)
         var h2 = $("<h2>")
-        var date =$("<h2>")
-        var temp =$("<p>")
+        var date = $("<h2>")
+        var temp = $("<p>")
         var humidity = $("<p>")
         var windSpeed = $("<p>")
-        windSpeed.text("Wind Speed: " +responce.wind.speed +" MPH")
-        humidity.text("Humidity: "+ responce.main.humidity + "%" )
-        temp.text("Temperature: " + ( Math.floor(Math.round(responce.main.temp *9/5) -459.67)) + " F")
-        date.text(" (" + dt.toDateString() +")")
-        date.attr("style","display:inline")
+        windSpeed.text("Wind Speed: " + responce.wind.speed + " MPH")
+        humidity.text("Humidity: " + responce.main.humidity + "%")
+        temp.text("Temperature: " + (Math.floor(Math.round(responce.main.temp * 9 / 5) - 459.67)) + " F")
+        date.text(" (" + dt.toDateString() + ")")
+        date.attr("style", "display:inline")
         h2.text(responce.name)
-        h2.attr("style","display:inline")
-        $("#today-weather").append(h2) 
-        $("#today-weather").append(date) 
-        $("#today-weather").append(temp) 
-        $("#today-weather").append(humidity) 
-        $("#today-weather").append(windSpeed) 
+        h2.attr("style", "display:inline")
+        $("#today-weather").append(h2)
+        $("#today-weather").append(date)
+        $("#today-weather").append(temp)
+        $("#today-weather").append(humidity)
+        $("#today-weather").append(windSpeed)
         $.ajax({
-            url:"http://api.openweathermap.org/data/2.5/uvi?appid={appid}&lat="+ responce.coord.lat +"&lon="+ responce.coord.lon + "&apikey=028bd6c65fc449153d91ad43fd573ddb",
-            method:"GET"
-            }).then(function(responce){
+            url: "http://api.openweathermap.org/data/2.5/uvi?appid={appid}&lat=" + responce.coord.lat + "&lon=" + responce.coord.lon + "&apikey=028bd6c65fc449153d91ad43fd573ddb",
+            method: "GET"
+        }).then(function (responce) {
             console.log(responce)
             var uvIndex = $("<p>")
             uvIndex.text("UV Index: " + responce.value)
-            $("#today-weather").append(uvIndex) 
-            
-            })
+            $("#today-weather").append(uvIndex)
+
+        })
     })
 }
+function fiveDayForcast(search) {
+ 
+    var h2 = $("<h2>")
+    h2.text("5-Day Forcast:")
+    $(".five-day-forcast").append(h2)
+    $.ajax({
+        url: "http://api.openweathermap.org/data/2.5/forecast/?q="+ search +",us&apikey=028bd6c65fc449153d91ad43fd573ddb",
+        method: "GET"
+    }).then(function (responce) {
+        console.log(responce)
+        for (var i = 0; i <40; i++) {
+            var div = $("<div>")
+            div.attr("style", "display:inline-block")
+            var floor = Math.floor(i =i +6.75)
+            var date = responce.list[floor].dt_txt
+            var setDate = new Date(date)
+            var splitDate = setDate.toDateString().split(" ")
+            splitDate.shift()
+            var joinedDate = splitDate.join(" ")
+            var p = $("<p>")
+            var temp = $("<p>")
+            var img = $("<img>")
+            img.attr("src", "http://openweathermap.org/img/w/" + responce.list[floor].weather[0].icon + ".png")
+            img.attr("alt", "Weather")
+            var humidity = $("<p>")
+            humidity.text("Humidity: " + responce.list[floor].main.humidity)
+            temp.text(("Temperature:" + Math.floor(Math.round(responce.list[floor].main.temp * 9 / 5) - 459.67)) + " F")
+            p.text(joinedDate)
+            div.append(p)
+            div.append(img)
+            div.append(temp)
+            div.append(humidity)
+            $(".five-day-forcast").append(div)
+        }
+    })
+}
+function displayButtons(){
+    var search = $("#search").val()
+    for (var i = 0; i < buttonArray.length; i++) {
+        var button = $("<button>")
+        button.attr("style","display:block")
+        button.text(buttonArray[i])    
+       $("#btn-storage").append(button)
+    }
+}
+displayButtons()
+var weather = {
+today:todayWeather,
+fiveDayForcast: fiveDayForcast
+}
 $("#btn-search").on("click", function () {
-
-todayWeather()
-var search = $("#search").val()
-   var h2 = $("<h2>")
-   h2.text("5-Day Forcast:")
-   $(".five-day-forcast").append(h2)
-   $.ajax({
-   url:"http://api.openweathermap.org/data/2.5/forecast?q="+ search +",usl&appid=028bd6c65fc449153d91ad43fd573ddb",
-   method:"GET"
-   }).then(function(responce){
-   console.log(responce)
-   }) 
-
+    var search = $("#search").val()
+    $(".five-day-forcast").empty()
+    $("#today-weather").empty()
+    weather.today(search)
+    weather.fiveDayForcast(search)
+     buttonArray.push(search)
+     localStorage.setItem("buttons",JSON.stringify(buttonArray))
+     console.log(buttonArray)
+     $("#search").val("")
+     $("#btn-storage").empty()
+    displayButtons()
 })
+$("#btn-storage").delegate("button","click",function(){
+$("#today-weather").empty()
+$(".five-day-forcast").empty()
+weather.today($(this).text())
+weather.fiveDayForcast($(this).text())
+})
+
